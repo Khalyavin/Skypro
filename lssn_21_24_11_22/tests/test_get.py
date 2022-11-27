377bca56ccf588bbca8d0aa9b0b3ee88304fc7f6
Khalyavin Vasily <STS2004@gmail.com>
2022-11-27 19:07:38 +0700

    Исходное задание

---
+++ lssn_21_24_11_22/tests/test_get.py
@@ -0,0 +1,6 @@
+from lssn_21_24_11_22.get import get
+
+def test_get():
+    assert get([1, 2, 3], 1, "a") == 2
+    assert get([4, 5, 6], 7, "val") == "val"
+
---
+++ lssn_21_24_11_22/tests/test_index_of.py
@@ -0,0 +1,5 @@
+from lssn_21_24_11_22.index_of import index_of
+
+def test_index_of():
+    assert index_of([2, 7, 3, 2, 4], 2) == 0
+
