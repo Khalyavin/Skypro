43cae1d235ea10d79ddd1c0da74c9277c74d9a45
Khalyavin Vasily <STS2004@gmail.com>
2022-11-30 01:26:30 +0700

    список готов

--- kurs_SP500/kurs_sp500/kurs_SP500.py
+++ kurs_SP500/kurs_sp500/kurs_SP500.py
@@ -1,17 +1,21 @@
 import csv
 
-full_path = '/home/vssl/PythonProject/kurs_SP500/all_stocks_5yr.csv'
+path = '/home/vssl/PythonProject/kurs_SP500/'
+data_file = 'all_stocks_5yr.csv'
+full_path = path + data_file
+
+data = []
+
 
 def read_data(limit_, group_by_name_, ticker_):
     """ Наполняет массив данными из файла в зависимости от флага group_by_name"""
-    data = []
 
     with open(full_path, 'r') as file:
         file_reader = csv.reader(file)
         cntr = 0
         for row in file_reader:
             if row[0] == 'date': continue
-            if cntr < limit_ + 1:
+            if cntr < limit_:
                 if not (group_by_name_ and ticker_):
                     data.append(row)
                 else:
@@ -20,10 +24,54 @@ def read_data(limit_, group_by_name_, ticker_):
                     else:
                         continue
                 cntr += 1
-                print(row)
             else:
                 break
-        print(data)
+
+
+def sort_data(sort_columns_, order_):
+    """ сортирует список data по полю
+     sort_columns: ["date", "open", "high", "low", "close", "volume"]
+    в порядке order: ["asc", "desc"]
+    """
+    # Разбираюсь что сортироавать
+    sort_col = ["date", "open", "high", "low", "close", "volume"]
+    if sort_columns_ == "date": col = 0
+    elif sort_columns_ == "open": col = 1
+    elif sort_columns_ == "high": col = 2
+    elif sort_columns_ == "low": col = 3
+    elif sort_columns_ == "close": col = 4
+    elif sort_columns_ == "volume":  # Остальные поля сортируются типом str, а это - переменной длинны
+        col = 5
+        for i in range(len(data)):
+            data[i][col] = int(data[i][col])
+    else:
+        return
+
+    # Так как массив "небольшой", реализую классику - bubble sort
+    cur_element = len(data)
+    while cur_element > 1:
+        for i in range(1, cur_element):
+            if data[i - 1][col] > data[i][col]:
+                data[i - 1], data[i] = data[i], data[i - 1]
+            else:
+                continue
+        cur_element -= 1
+    for i in range(len(data)):
+        data[i][col] = str(data[i][col])
+        # Теперь в data все элементы - string, готовы к dump to file
+
+    if order_ == 'desc':  # Переворачиваю массив data
+        for i in range(len(data) // 2):
+            data[i], data[len(data) - i - 1] = data[len(data) - i - 1], data[i]
+
+
+def write_data(f_name):
+    if not f-name:  # В запросе нет файла, вывожу на экран
+        for i in range(len(data)):
+            print(data[i])
+    else:
+        pass
+
 
 def select_sorted(sort_columns=None, limit=20, order='asc', group_by_name=True,
                   ticker=None, filename='dump.csv'):
@@ -37,6 +85,13 @@ def select_sorted(sort_columns=None, limit=20, order='asc', group_by_name=True,
     """
 
     read_data(limit, group_by_name, ticker)
+    # в списке data лежит limit строк по условию group_by_name
+
+    sort_data(sort_columns, order)
+    # в списке data лежат отсортированные limit строк по условию order
+
+    write_data(filename)
+    # отсортированные данные сброшены в файл filename
 
 
-select_sorted(limit=5, ticker='FLS', group_by_name=False)
+select_sorted(sort_columns="volume", order="desc", limit=5, ticker='FLS', group_by_name=True)
