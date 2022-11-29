2f3a6329bc2739d8706fb554af51f0fbf695c2e1
Khalyavin Vasily <STS2004@gmail.com>
2022-11-29 22:38:35 +0700

    наполнен из файла по условию список

--- kurs_SP500/kurs_sp500/kurs_SP500.py
+++ kurs_SP500/kurs_sp500/kurs_SP500.py
@@ -1,5 +1,30 @@
 import csv
 
+full_path = '/home/vssl/PythonProject/kurs_SP500/all_stocks_5yr.csv'
+
+def read_data(limit_, group_by_name_, ticker_):
+    """ Наполняет массив данными из файла в зависимости от флага group_by_name"""
+    data = []
+
+    with open(full_path, 'r') as file:
+        file_reader = csv.reader(file)
+        cntr = 0
+        for row in file_reader:
+            if row[0] == 'date': continue
+            if cntr < limit_ + 1:
+                if not (group_by_name_ and ticker_):
+                    data.append(row)
+                else:
+                    if row[6] == ticker_:
+                        data.append(row)
+                    else:
+                        continue
+                cntr += 1
+                print(row)
+            else:
+                break
+        print(data)
+
 def select_sorted(sort_columns=None, limit=20, order='asc', group_by_name=True,
                   ticker=None, filename='dump.csv'):
     """ Функция выбора и сортировки данных по S&P_500. Параметры:
@@ -11,14 +36,7 @@ def select_sorted(sort_columns=None, limit=20, order='asc', group_by_name=True,
     fliename: // файл для сохранения полученной выборки
     """
 
-    with open('/home/vssl/PythonProject/kurs_SP500/all_stocks_5yr.csv', 'r') as file:
-        file_reader = csv.reader(file)
-        cntr = 0
-        for row in file_reader:
-             if cntr < limit + 1:
-                cntr += 1
-                print(row)
-             else:
-                 break
+    read_data(limit, group_by_name, ticker)
 
-select_sorted(limit=5)
+
+select_sorted(limit=5, ticker='FLS', group_by_name=False)
